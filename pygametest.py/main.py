2fc805e43a91630b741c7877d252996e2cb0cee7
MethMicha <michael.k.lindner@t-online.de>
2022-10-09 20:56:33 +0200

    Spaceinvador

--- pygametest.py/main.py
+++ pygametest.py/main.py
@@ -4,26 +4,36 @@ pygame.init()
 
 #Bildfläche definieren
 screen = pygame.display.set_mode((800, 600))       # () - einfügen -> darin Bildfläche erstellen
-background = pygame.image.load("galaxie.jpg")
+background = pygame.image.load("graphics/galaxie.jpg")
 score_tile = pygame.Surface((800,50))
 
 #Titel & Icon
 pygame.display.set_caption("Spaceinvador")          # Titel des Fensters festlegen
-icon = pygame.image.load("ufo.png")                 # Icon für das fenster festlegen
+icon = pygame.image.load("graphics/ufo.png")                 # Icon für das fenster festlegen
 pygame.display.set_icon(icon)
 
 #Clock                                              # Clock für die Framerate festlegen
 clock = pygame.time.Clock()
 
+# Fonts
+test_font = pygame.font.Font("fonts/PixeloidSans.ttf", 30)                 # Eine Font erstellen -> (Font, größe)
+
+#Score
+score_val = 0
+score = test_font.render(f"Score:  {score_val}", False, "White")           # rendern von Schriften -> ("Was möchte ich rendern", Antialiasing(Kantensmoothnes), Color)
+
 #Player
-playerImg = pygame.image.load("raumschiff.png")
+playerImg = pygame.image.load("graphics/raumschiff.png")
 playerX = 370
 playerY = 480
 
 #Endboss
-endbossImg = pygame.image.load("monster.png")
+endbossImg = pygame.image.load("graphics/monster.png")
 endbossX = 40
 endbossY = 370
+
+left_an = False
+righ_an = False
 
 def player(playerImg,playerX,playerY):
     screen.blit(playerImg, (playerX, playerY))                             # .blit = synonüm für drawing
@@ -42,8 +52,15 @@ while running:
     screen.blit(background,(0,0))                                           # Background textur draw
     screen.blit(score_tile, (0,550))                                        # Score Tile draw
 
+    #Movement Endboss
+
+
     endboss(endbossImg,endbossY, endbossX)                                  # Endboss draw
+
+
     player(playerImg,playerX,playerY)                                       # Player draw
+
+    screen.blit(score, (20, 555))
 
     pygame.display.update()                                                 # Bildschirm mit änderungen updaten -> nach jeden Gameloop durchlauf
     clock.tick(60)                                                          # legt die maximale Framerate auf 60 FPS fest -> while loop wird max 60 mal pro sekunde wiederholt
